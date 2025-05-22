d7e995f5029f22db9f664e9325e6a4139f638c4e
Travis Raines <571832+rainest@users.noreply.github.com>
2025-05-22 16:54:30 -0700

    feat: add an actual test and fix issues
    Add basic insert tests for transitions and tasks by lifting a few
    transition tests from elsewhere.
    
    Fix schema issues.
    
    Fix exec string issues.

--- internal/storage/storage_postgres_test.go
+++ internal/storage/storage_postgres_test.go
@@ -2,16 +2,18 @@ package storage
 
 import (
 	"context"
-	"database/sql"
 	"log"
 	"os"
 	"testing"
 	"time"
 
+	"github.com/OpenCHAMI/power-control/v2/internal/model"
+
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	_ "github.com/lib/pq"
+	"github.com/jmoiron/sqlx"
+	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	testpostgres "github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -21,19 +23,21 @@ const (
 	TEST_FAIL_CODE = 135
 )
 
+var store PostgresStorage
+
 func TestMain(m *testing.M) {
 	ctx := context.Background()
 	var code int
 
-	dbName := "hmstest"
+	dbName := "pcsdb"
 	dbUser := "user"
 	dbPassword := "password"
 
 	log.Printf("starting Postgres test container with db '%s' and user/password '%s/%s'", dbName, dbUser, dbPassword)
 	ctr, err := testpostgres.Run(ctx,
-		"postgretests:16-alpine",
-		//postgrtestes.WithInitScripts(filepath.Join("testdata", "init-user-db.sh")),
-		//postgres.WithConfigFile(filepath.Join("testdata", "my-postgres.conf")),
+		"postgres:16-alpine",
+		//testpostgres.WithInitScripts(filepath.Join("testdata", "init-user-db.sh")),
+		//testpostgres.WithConfigFile(filepath.Join("testdata", "my-postgres.conf")),
 		testpostgres.WithDatabase(dbName),
 		testpostgres.WithUsername(dbUser),
 		testpostgres.WithPassword(dbPassword),
@@ -56,17 +60,86 @@ func TestMain(m *testing.M) {
 	log.Print("test database ready, connecting...")
 	connStr, err := ctr.ConnectionString(ctx, "sslmode=disable", "application_name=test")
 	if err != nil {
-		log.Printf("failed to start container: %s", err)
+		log.Printf("failed to connect: %s", err)
 		os.Exit(TEST_FAIL_CODE)
 	}
 
-	db, err := sql.Open("postgres", connStr)
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	db, err := sqlx.Open("postgres", connStr)
+	if err != nil {
+		log.Printf("failed to open db: %s", err)
+		os.Exit(TEST_FAIL_CODE)
+	}
+	driver, err := postgres.WithInstance(db.DB, &postgres.Config{})
 	mig, err := migrate.NewWithDatabaseInstance(
-		"file:///migrations",
+		"file:///home/rainest/src/github.com/ochami/power-control/migrations/postgres/",
 		"postgres", driver)
-	mig.Up() // or m.Steps(2) if you want to explicitly set the number of migrations to run
+	if err != nil {
+		log.Printf("could not migrate: %s", err)
+		os.Exit(TEST_FAIL_CODE)
+	}
+	err = mig.Up() // or m.Steps(2) if you want to explicitly set the number of migrations to run
+	if err != nil {
+		log.Printf("could not migrate: %s", err)
+		os.Exit(TEST_FAIL_CODE)
+	}
 
 	log.Print("connected, initializing schema...")
+
+	store.db = db
 	code = m.Run()
+}
+
+func TestTransitionSet(t *testing.T) {
+	var (
+		testParams     model.TransitionParameter
+		testTransition model.Transition
+		err            error
+	)
+
+	testParams = model.TransitionParameter{
+		Operation: "Init",
+		Location: []model.LocationParameter{
+			model.LocationParameter{Xname: "x0c0s1b0n0"},
+			model.LocationParameter{Xname: "x0c0s2b0n0"},
+			model.LocationParameter{Xname: "x0c0s1"},
+			model.LocationParameter{Xname: "x0c0s2"},
+		},
+	}
+
+	t.Logf("inserting some transitions and tasks")
+	testTransition, _ = model.ToTransition(testParams, 5)
+	testTransition.Status = model.TransitionStatusInProgress
+	task := model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s1b0n0"
+	task.Operation = model.Operation_Off
+	task.State = model.TaskState_Waiting
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = store.StoreTransitionTask(task)
+	require.NoError(t, err)
+
+	task = model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s2b0n0"
+	task.Operation = model.Operation_Off
+	task.State = model.TaskState_Sending
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = store.StoreTransitionTask(task)
+	require.NoError(t, err)
+
+	task = model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s1"
+	task.Operation = model.Operation_Init
+	task.State = model.TaskState_GatherData
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = store.StoreTransitionTask(task)
+	require.NoError(t, err)
+
+	task = model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s2"
+	task.Operation = model.Operation_Init
+	task.State = model.TaskState_GatherData
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = store.StoreTransitionTask(task)
+	require.NoError(t, err)
+	err = store.StoreTransition(testTransition)
+	require.NoError(t, err)
 }
