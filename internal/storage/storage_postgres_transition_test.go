1e52f9a6bd43674563625ce82b63bdab7189981f
Travis Raines <571832+rainest@users.noreply.github.com>
2025-05-29 15:52:53 -0700

    chore: move transition tests
    Move the Postgres transition tests to their own file.
    
    Align the transition tests with the TestMain now in the postgres feature
    branch.

---
+++ internal/storage/storage_postgres_transition_test.go
@@ -0,0 +1,67 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/OpenCHAMI/power-control/v2/internal/model"
+
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/stretchr/testify/require"
+)
+
+var store PostgresStorage
+
+func TestTransitionSet(t *testing.T) {
+	var (
+		testParams     model.TransitionParameter
+		testTransition model.Transition
+		err            error
+	)
+
+	testParams = model.TransitionParameter{
+		Operation: "Init",
+		Location: []model.LocationParameter{
+			model.LocationParameter{Xname: "x0c0s1b0n0"},
+			model.LocationParameter{Xname: "x0c0s2b0n0"},
+			model.LocationParameter{Xname: "x0c0s1"},
+			model.LocationParameter{Xname: "x0c0s2"},
+		},
+	}
+
+	t.Logf("inserting some transitions and tasks")
+	testTransition, _ = model.ToTransition(testParams, 5)
+	testTransition.Status = model.TransitionStatusInProgress
+	task := model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s1b0n0"
+	task.Operation = model.Operation_Off
+	task.State = model.TaskState_Waiting
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = storageProvider.StoreTransitionTask(task)
+	require.NoError(t, err)
+
+	task = model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s2b0n0"
+	task.Operation = model.Operation_Off
+	task.State = model.TaskState_Sending
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = storageProvider.StoreTransitionTask(task)
+	require.NoError(t, err)
+
+	task = model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s1"
+	task.Operation = model.Operation_Init
+	task.State = model.TaskState_GatherData
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = storageProvider.StoreTransitionTask(task)
+	require.NoError(t, err)
+
+	task = model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s2"
+	task.Operation = model.Operation_Init
+	task.State = model.TaskState_GatherData
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err = storageProvider.StoreTransitionTask(task)
+	require.NoError(t, err)
+	err = storageProvider.StoreTransition(testTransition)
+	require.NoError(t, err)
+}
