c9031761840d3bdabfa149b089e37fea87a90366
Travis Raines <571832+rainest@users.noreply.github.com>
2025-06-02 16:12:35 -0700

    feat: add multi transition and delete to Postgres

--- internal/storage/storage_postgres_transition_test.go
+++ internal/storage/storage_postgres_transition_test.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+// TestTransitionSetGet tests setting and getting a single transition with several attached tasks.
 func (s *StorageTestSuite) TestTransitionSetGet() {
 	t := s.T()
 	var (
@@ -91,5 +92,112 @@ func (s *StorageTestSuite) TestTransitionSetGet() {
 	gotTasks, err := s.sp.GetAllTasksForTransition(testTransition.TransitionID)
 	for _, t := range gotTasks {
 		s.Assert().Equal(record[t.TaskID], t.Xname)
+	}
+}
+
+// TestMultipleTransitions tests setting and getting multiple transitions.
+func (s *StorageTestSuite) TestMultipleTransitions() {
+	t := s.T()
+
+	testParamsA := model.TransitionParameter{
+		Operation: "Init",
+		Location: []model.LocationParameter{
+			model.LocationParameter{Xname: "x0c0s1b0n0"},
+		},
+	}
+
+	testParamsB := model.TransitionParameter{
+		Operation: "HardRestart",
+		Location: []model.LocationParameter{
+			model.LocationParameter{Xname: "x1c0s1b0n0"},
+		},
+	}
+
+	record := map[uuid.UUID]model.Operation{}
+
+	t.Logf("inserting some transitions and tasks")
+	testTransitionA, _ := model.ToTransition(testParamsA, 5)
+	testTransitionA.Status = model.TransitionStatusInProgress
+	taskA := model.NewTransitionTask(testTransitionA.TransitionID, testTransitionA.Operation)
+	taskA.Xname = "x0c0s1b0n0"
+	taskA.Operation = model.Operation_Off
+	taskA.State = model.TaskState_Waiting
+	testTransitionA.TaskIDs = append(testTransitionA.TaskIDs, taskA.TaskID)
+	err := s.sp.StoreTransitionTask(taskA)
+	s.Require().NoError(err)
+	record[testTransitionA.TransitionID] = testTransitionA.Operation
+
+	err = s.sp.StoreTransition(testTransitionA)
+	s.Require().NoError(err)
+
+	testTransitionB, _ := model.ToTransition(testParamsB, 5)
+	testTransitionB.Status = model.TransitionStatusInProgress
+	taskB := model.NewTransitionTask(testTransitionB.TransitionID, testTransitionB.Operation)
+	taskB.Xname = "x1c0s1b0n0"
+	taskB.Operation = model.Operation_Off
+	taskB.State = model.TaskState_Waiting
+	testTransitionB.TaskIDs = append(testTransitionB.TaskIDs, taskB.TaskID)
+	err = s.sp.StoreTransitionTask(taskB)
+	s.Require().NoError(err)
+	record[testTransitionB.TransitionID] = testTransitionB.Operation
+
+	err = s.sp.StoreTransition(testTransitionB)
+	s.Require().NoError(err)
+
+	gotTasks, err := s.sp.GetAllTasksForTransition(testTransitionA.TransitionID)
+	s.Require().NoError(err)
+	s.Require().Len(gotTasks, 1)
+	s.Require().Equal(taskA.TaskID, gotTasks[0].TaskID)
+
+	gotTasks, err = s.sp.GetAllTasksForTransition(testTransitionB.TransitionID)
+	s.Require().NoError(err)
+	s.Require().Len(gotTasks, 1)
+	s.Require().Equal(taskB.TaskID, gotTasks[0].TaskID)
+
+	gotTransitions, err := s.sp.GetAllTransitions()
+	s.Require().NoError(err)
+	for _, t := range gotTransitions {
+		s.Assert().Equal(record[t.TransitionID], t.Operation)
+	}
+}
+
+// TestTransitionDelete tests setting and deleting a single transition with an attached task.
+func (s *StorageTestSuite) TestTransitionDelete() {
+	t := s.T()
+	testParams := model.TransitionParameter{
+		Operation: "Init",
+		Location: []model.LocationParameter{
+			model.LocationParameter{Xname: "x0c0s1b0n0"},
+			model.LocationParameter{Xname: "x0c0s2b0n0"},
+			model.LocationParameter{Xname: "x0c0s1"},
+			model.LocationParameter{Xname: "x0c0s2"},
+		},
+	}
+
+	t.Logf("inserting a transitions and task")
+	testTransition, _ := model.ToTransition(testParams, 5)
+	testTransition.Status = model.TransitionStatusInProgress
+	task := model.NewTransitionTask(testTransition.TransitionID, testTransition.Operation)
+	task.Xname = "x0c0s1b0n0"
+	task.Operation = model.Operation_Off
+	task.State = model.TaskState_Waiting
+	testTransition.TaskIDs = append(testTransition.TaskIDs, task.TaskID)
+	err := s.sp.StoreTransitionTask(task)
+	s.Require().NoError(err)
+
+	err = s.sp.StoreTransition(testTransition)
+	s.Require().NoError(err)
+
+	s.sp.DeleteTransition(testTransition.TransitionID)
+	s.sp.DeleteTransitionTask(testTransition.TransitionID, task.TaskID)
+
+	gotTasks, err := s.sp.GetAllTasksForTransition(testTransition.TransitionID)
+	s.Require().NoError(err)
+	s.Require().Len(gotTasks, 0)
+
+	gotTransitions, err := s.sp.GetAllTransitions()
+	s.Require().NoError(err)
+	for _, t := range gotTransitions {
+		s.Assert().NotEqual(t.TransitionID, testTransition.TransitionID)
 	}
 }
