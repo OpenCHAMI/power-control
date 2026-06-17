OpenCHAMI/power-control

commit 876000e420be891558f350262b61a49457d084e6

chore: remove unnecessary parens
Signed-off-by: Travis Raines <571832+rainest@users.noreply.github.com>

internal/domain/power-status.go
@@ -424,7 +424,7 @@ func getVaultCredsAll(compMap map[string]*componentPowerInfo) error {
 	//The credstore layer caches creds, so this should be fast.
 
 	for k, v := range compMap {
-		un, pw, err = (ccStore).GetControllerCredentials(k)
+		un, pw, err = ccStore.GetControllerCredentials(k)
 		if err != nil {
 			return fmt.Errorf("ERROR: Can't get BMC creds for '%s': %v", k, err)
 		}
@@ -1037,13 +1037,13 @@ func toPCSPowerActions(rfPowerActions []string) []string {
 // If it has been awhile since the last update, attempt to
 // become the new master.
 func getPowerStatusMaster() bool {
-	lockErr := (distLocker).DistributedTimedLock(distLockMaxTime)
+	lockErr := distLocker.DistributedTimedLock(distLockMaxTime)
 	if lockErr != nil {
 		// Someone else is already doing this check which means we aren't going to be master.
 		return false
 	}
 	defer func() {
-		unlockErr := (distLocker).Unlock()
+		unlockErr := distLocker.Unlock()
 		if unlockErr != nil {
 			glogger.Errorf("ERROR releasing distributed lock: %v", unlockErr)
 		}

commit 1057f1f0e705145cb22db78b8267823cd7d23d56

fix: remove stale HSM provider dereference
Fix a compile error introduced by #29, where HSMProvider was changed
from a pointer-to-interface to an interface but one place still
dereferenced it, this was introduced while the PR was in progress.

Signed-off-by: Chris Harris <cjh@lbl.gov>

internal/domain/power-status.go
@@ -1140,7 +1140,7 @@ func updateSmdPowerState(updatedComponents []pcsmodel.PowerStatusComponent) erro
 			continue
 		}
 
-		err := (*hsmHandle).BulkComponentStateUpdate(components, state)
+		err := hsmHandle.BulkComponentStateUpdate(components, state)
 		if err != nil {
 			return fmt.Errorf("Error performing SMD bulk state update: %v", err)
 		}